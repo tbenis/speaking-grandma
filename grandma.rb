f2d24469f59666362045f4e9986b29d581d2eba0
Benis Tambe <will.jenifer@yahoo.com>
2021-01-13 00:31:50 -0500

    commit tree 4630c837b2d2ee4e4df9629c75b82f37e61d76e9

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
             phrase = "I LOVE YOU TOO PUMPKIN!"
         else
                 puts "NO, NOT SINCE 1938!"
-                phrase = "NO, NOT SINCE 1938!"
+                phrase  = "NO, NOT SINCE 1938!"
         end
     else
         puts "HUH?! SPEAK UP, SONNY!"
